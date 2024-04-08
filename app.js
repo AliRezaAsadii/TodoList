2748cba39f70291f73e688d97d78166e4e7304ce
alirezaAsadii <alirezaasadi7484@gmail.com>
2024-04-08 22:36:09 +0330

    create and connect database

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ const express = require("express");
 const morgan = require("morgan");
 
 const app = express();
+
+//* middleware
+
+process.env.NODE_ENV === "development" && app.use(morgan("dev"));
+
+app.use(express.json());
+
+module.exports = app;
